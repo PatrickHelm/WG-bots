23f5f4ed49e8b0b77aa3b4a9aed4ac193299b503
Your Name <you@example.com>
2023-09-24 13:23:40 +0000

    update search string

--- wgwohnungsbot/wgwohnungsbot/spiders/wg-gesucht-spider.py
+++ wgwohnungsbot/wgwohnungsbot/spiders/wg-gesucht-spider.py
@@ -3,7 +3,7 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "wg-gesucht"
     start_urls = [
-                'https://www.wg-gesucht.de/1-zimmer-wohnungen-in-Muenchen.90.1.1.0.html?offer_filter=1&city_id=90&sort_order=0&noDeact=1&dFr=1698793200&categories%5B%5D=1&rent_types%5B%5D=2&rMax=650&exc=2',
+                'https://www.wg-gesucht.de/1-zimmer-wohnungen-und-wohnungen-in-Muenchen.90.1+2.1.0.html?offer_filter=1&city_id=90&sort_order=0&noDeact=1&dFr=1701428400&categories%5B%5D=1&categories%5B%5D=2&rent_types%5B%5D=2&sMin=18&rMax=650&exc=2',
             ]
 
     def parse(self, response):
